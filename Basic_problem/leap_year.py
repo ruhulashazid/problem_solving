cdf56e91c13dedd5c80bf07225ba2749b702bb07
ruhulashazid <ruhulaminshazid@gmail.com>
2024-05-12 01:30:51 +0600

    Basic problem

---
+++ Basic_problem/leap_year.py
@@ -0,0 +1,16 @@
+#This program checks if a year is a leap year or not.
+"""
+year = int(input("Enter a year: "))
+
+if year % 4 !=0:
+    print("Common year")
+else:
+    if year % 100 != 0:
+        if year % 400 != 0:
+            print("Leap year")
+        else:
+            print("Common year")
+    else:
+        print("Leap year")       
+        
+"""
